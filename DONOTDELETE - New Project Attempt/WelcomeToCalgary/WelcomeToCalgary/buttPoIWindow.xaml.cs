4df82be2e91d6f06ee990a703c80837a58199beb
djarmstr1 <djarmstr@ucalgary.ca>
2016-03-06 17:55:17 -0700

    Creation of PoI window

---
+++ DONOTDELETE - New Project Attempt/WelcomeToCalgary/WelcomeToCalgary/buttPoIWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WelcomeToCalgary
+{
+    /// <summary>
+    /// Interaction logic for buttPoIWindow.xaml
+    /// </summary>
+    public partial class buttPoIWindow : Window
+    {
+        public buttPoIWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
