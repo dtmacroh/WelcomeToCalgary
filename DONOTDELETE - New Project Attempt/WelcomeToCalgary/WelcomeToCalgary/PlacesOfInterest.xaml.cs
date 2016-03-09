bfc4bdf7408271cf12d751c822fd6bec9c2038e4
djarmstr1 <djarmstr@ucalgary.ca>
2016-03-08 19:37:41 -0700

    Pages now link together properly; basic header to make page change obvious

--- DONOTDELETE - New Project Attempt/WelcomeToCalgary/WelcomeToCalgary/PlacesOfInterest.xaml.cs
+++ DONOTDELETE - New Project Attempt/WelcomeToCalgary/WelcomeToCalgary/PlacesOfInterest.xaml.cs
@@ -20,43 +20,65 @@ namespace WelcomeToCalgary
     /// </summary>
     public partial class PlacesOfInterest : UserControl
     {
+        Panel frameX;
         public PlacesOfInterest()
         {
             InitializeComponent();
         }
         private void buttPoITrending_Click(object sender, RoutedEventArgs e)
         {
-
+            frameX = (this.Parent as Panel);
+            PoITrending newPage = new PoITrending();
+            frameX.Children.Clear();
+            frameX.Children.Add(newPage);
         }
 
         private void buttPoIRestaurants_Click(object sender, RoutedEventArgs e)
         {
-
+            frameX = (this.Parent as Panel);
+            PoIRestaurant newPage = new PoIRestaurant();
+            frameX.Children.Clear();
+            frameX.Children.Add(newPage);
         }
 
         private void buttPoIRecreational_Click(object sender, RoutedEventArgs e)
         {
-
+            frameX = (this.Parent as Panel);
+            PoIRecreation newPage = new PoIRecreation();
+            frameX.Children.Clear();
+            frameX.Children.Add(newPage);
         }
 
         private void buttPoINature_Click(object sender, RoutedEventArgs e)
         {
-
+            frameX = (this.Parent as Panel);
+            PoINature newPage = new PoINature();
+            frameX.Children.Clear();
+            frameX.Children.Add(newPage);
         }
 
         private void buttPoISports_Click(object sender, RoutedEventArgs e)
         {
-
+            frameX = (this.Parent as Panel);
+            PoISports newPage = new PoISports();
+            frameX.Children.Clear();
+            frameX.Children.Add(newPage);
         }
 
         private void buttPoIArts_Click(object sender, RoutedEventArgs e)
         {
-
+            frameX = (this.Parent as Panel);
+            PoIArts newPage = new PoIArts();
+            frameX.Children.Clear();
+            frameX.Children.Add(newPage);
         }
 
         private void buttPoIReligion_Click(object sender, RoutedEventArgs e)
         {
-
+            frameX = (this.Parent as Panel);
+            PoIReligion newPage = new PoIReligion();
+            frameX.Children.Clear();
+            frameX.Children.Add(newPage);
         }
     }
 }
