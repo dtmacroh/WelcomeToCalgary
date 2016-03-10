f6cf32c61f0a26b369b7bb085232f4034b0ef6af
debbie <dmac114@gmail.com>
2016-03-09 21:59:48 -0700

    secondary user controls containing calgary info

---
+++ DONOTDELETE -debbie Copy/WelcomeToCalgary/WelcomeToCalgary/CalgaryInfo.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WelcomeToCalgary
+{
+    /// <summary>
+    /// Interaction logic for CalgaryInfo.xaml
+    /// </summary>
+    public partial class CalgaryInfo : UserControl
+    {
+        StackPanel mainScroll;
+        public CalgaryInfo(StackPanel scroll)
+        {
+            InitializeComponent();
+            mainScroll = scroll;
+        }
+        public void ButtHistory_Click(object sender, RoutedEventArgs e)
+        {
+            CIHistory area = new CIHistory(mainScroll);
+            mainScroll.Children.Clear();
+            mainScroll.Children.Add(area);
+        }
+        public void ButtCulture_Click(object sender, RoutedEventArgs e)
+        {
+            CICulture area = new CICulture(mainScroll);
+            mainScroll.Children.Clear();
+            mainScroll.Children.Add(area);
+        }
+        public void ButtSports_Click(object sender, RoutedEventArgs e)
+        {
+            CISports area = new CISports(mainScroll);
+            mainScroll.Children.Clear();
+            mainScroll.Children.Add(area);
+        }
+        public void ButtTop10_Click(object sender, RoutedEventArgs e)
+        {
+            CITop10 area = new CITop10(mainScroll);
+            mainScroll.Children.Clear();
+            mainScroll.Children.Add(area);
+        }
+    }
+}
---
+++ DONOTDELETE -debbie Copy/WelcomeToCalgary/WelcomeToCalgary/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WelcomeToCalgary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttPoI_Click(object sender, RoutedEventArgs e)
+        {
+            PlacesOfInterest interest = new PlacesOfInterest();
+            MainScroll.Children.Clear();
+            MainScroll.Children.Add(interest);
+        }
+
+        private void buttCalInfo_Click(object sender, RoutedEventArgs e)
+        {
+            CalgaryInfo calgaryInfo = new CalgaryInfo(MainScroll);
+            MainScroll.Children.Clear();
+            MainScroll.Children.Add(calgaryInfo);
+        }
+
+        private void buttEvents_Click(object sender, RoutedEventArgs e)
+        {
+            EventsMain events = new EventsMain(MainScroll);
+            MainScroll.Children.Clear();
+            MainScroll.Children.Add(events);
+        }
+
+        private void buttLinks_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
