a9f9525a50920fc59aed1e3174b70066b3bbdc67
djarmstr1 <djarmstr@ucalgary.ca>
2016-03-10 18:29:58 -0700

    Will this ruin us? Adding everything in the folder to the 'changes made' list

---
+++ UserControlExercise/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 20;i++ )
+            {
+                Email newMail = new Email();
+                newMail.Sender = "Sender Number " + i.ToString();
+                newMail.Body = "This is message number " + i.ToString() + ". Good luck.";
+                emailsList.Children.Add(newMail);
+            }
+        }
+    }
+}
