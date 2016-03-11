275a8fc0075edd4d475e907559903c8a87bb10f1
huynjm <huynjm@ucalgary.ca>
2016-03-10 19:49:47 -0700

    done events

--- DONOTDELETE - New Project Attempt/WelcomeToCalgary/WelcomeToCalgary/EventAreaSelect.xaml.cs
+++ DONOTDELETE - New Project Attempt/WelcomeToCalgary/WelcomeToCalgary/EventAreaSelect.xaml.cs
@@ -36,20 +36,21 @@ namespace WelcomeToCalgary
         private void wsprings_Selected(object sender, RoutedEventArgs e)
         {
             scrollb.Visibility = Visibility.Visible;
+            stacklist.Children.Clear();
             for (int i = 0; i < 2; i++)
             {
                 EventCreator Event = new EventCreator();
                 if (i == 0)
                 {
-                    Event.Title = "Easter Hunt";
+                    Event.Title = "Find the Eastern Eggs";
                     Event.Details = "Welcome to the annual egg hunt, open for all ages. There will be a variety of treats hidden in" +
-                    "the smallest corners of the field. " + "\n" + "Date & Time: April 5, 2016" + "\n" + "Location: Millow Park";
+                    "the smallest corners of the field. " + "\n" + "Date & Time: April 5, 2016 @ 12:30PM" + "\n" + "Location: Millow Park";
                 }
                 else
                 {
-                    Event.Title = "Petting Zoo";
+                    Event.Title = "Zoolander";
                     Event.Details = "Come visit the latest petting zoo with your children to let them play with the tame animals" +
-                                        "Date & Time: March 30, 2016"+ "\n" + "Location: Marshmellow Centre";
+                                        "Date & Time: March 30, 2016 @ 11AM"+ "\n" + "Location: Marshmellow Centre";
                 }
 
                 this.stacklist.Children.Add(Event);
@@ -60,12 +61,50 @@ namespace WelcomeToCalgary
         private void forest_Selected(object sender, RoutedEventArgs e)
         {
             scrollb.Visibility = Visibility.Visible;
+            stacklist.Children.Clear();
+            for (int i = 0; i < 2; i++)
+            {
+                EventCreator Event = new EventCreator();
+                if (i == 0)
+                {
+                    Event.Title = "Car Showoff";
+                    Event.Details = "Come show off your car and compete in the annual contest. Special guest judge." +
+                     "\n" + "Date & Time: May 1, 2016 @ 10AM" + "\n" + "Location: Lawson Shopping Mall";
+                }
+                else
+                {
+                    Event.Title = "Hot Dog Race";
+                    Event.Details = "Enter your dog to race with other fellow dogs. Hot dogs offered at lunch" +
+                                        "Date & Time: April 30, 2016 @ 12PM" + "\n" + "Location: West Hill";
+                }
+
+                this.stacklist.Children.Add(Event);
+            }
 
         }
 
         private void uni_Selected(object sender, RoutedEventArgs e)
         {
             scrollb.Visibility = Visibility.Visible;
+            stacklist.Children.Clear();
+            for (int i = 0; i < 2; i++)
+            {
+                EventCreator Event = new EventCreator();
+                if (i == 0)
+                {
+                    Event.Title = "Dance Off";
+                    Event.Details = "Come compete in the 12HR dance marathon. Bring your partner and your best shoes." +
+                     "\n" + "Date & Time: July 25, 2016 @ 9AM" + "\n" + "Location: Market Community Centre";
+                }
+                else
+                {
+                    Event.Title = "Roaring Dinos";
+                    Event.Details = "Come support the local basketball teams as they compete as they become the very best" +
+                                        "Date & Time: July 30, 2016 @ 5PM" + "\n" + "Location: Unison Centre";
+                }
+
+                this.stacklist.Children.Add(Event);
+            }
 
         }
     }
--- DONOTDELETE - New Project Attempt/WelcomeToCalgary/WelcomeToCalgary/EventCultureSelect.xaml.cs
+++ DONOTDELETE - New Project Attempt/WelcomeToCalgary/WelcomeToCalgary/EventCultureSelect.xaml.cs
@@ -36,18 +36,76 @@ namespace WelcomeToCalgary
         private void spain_Selected(object sender, RoutedEventArgs e)
         {
             scroll.Visibility = Visibility.Visible;
+            stackpanie.Children.Clear();
+            for (int i = 0; i < 2; i++)
+            {
+                EventCreator Event = new EventCreator();
+                if (i == 0)
+                {
+                    Event.Title = "Fandango";
+                    Event.Details = "Attend a lesson with a partner and experience a famous dance of Spain." +
+                     "\n" + "Date & Time: March 28, 2016" + "\n" + "Location: Hudson Community Centre, Brentwood";
+                }
+                else
+                {                         
+                    Event.Title = "Feast for the Beast";
+                    Event.Details = "Feast on the delicious food offered by local food trucks with your tradition spain cuisine" +
+                                        "Date & Time:  May 22, 2016 @ 12PM" + "\n" + "Location: 12th Langdon Ave, Downtown";
+                }
+
+                this.stackpanie.Children.Add(Event);
+            }
 
         }
 
         private void france_Selected(object sender, RoutedEventArgs e)
         {
             scroll.Visibility = Visibility.Visible;
+            stackpanie.Children.Clear();
+            for (int i = 0; i < 2; i++)
+            {
+                EventCreator Event = new EventCreator();
+                if (i == 0)
+                {
+                    Event.Title = "Walking for the Cure";
+                    Event.Details = "Participate in the upcoming marathon to raise money in hopes of curing cancer." +
+                    "\n" + "Date & Time: May 6, 2016 @ 9AM" + "\n" + "Location: Easton Park, Temple";
+                }
+                else
+                {
+           
+                    Event.Title = "Oui Oui Paintnight";
+                    Event.Details = "Come drop by for the night and learn how to paint the Eiffel Tower" +
+                                        "Date & Time: June 5, 2016 @ 6PM" + "\n" + "Location: Louise Painthall, Kensington";
+                }
+
+                this.stackpanie.Children.Add(Event);
+            }
 
         }
 
         private void japan_Selected(object sender, RoutedEventArgs e)
         {
             scroll.Visibility = Visibility.Visible;
+            stackpanie.Children.Clear();
+            for (int i = 0; i < 2; i++)
+            {
+                EventCreator Event = new EventCreator();
+                if (i == 0)
+                {
+                    Event.Title = "Taekwondo Duo";
+                    Event.Details = "The local dojo will be performing a beautiful performance with a special appearance by two upraising performers." +
+                     "\n" + "Date & Time: February 28, 2016 @ 1PM" + "\n" + "Location: Frazer Park, Brentwood";
+                }
+                else
+                {
+                    Event.Title = "Dragon Dance";
+                    Event.Details = "Participate in the annual dragon dance and experience the fiery moves. Bonfire and fireworks during the evening. " +
+                                        "Date & Time: February 20, 2016 @ 4PM" + "\n" + "Location: Dragon City, Downtown";
+                }
+
+                this.stackpanie.Children.Add(Event);
+            }
 
         }
     }
