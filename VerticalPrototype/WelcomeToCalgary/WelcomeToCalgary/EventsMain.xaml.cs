ee059df39024745c7bf15470f419ea86295c9d54
debbie <dmac114@gmail.com>
2016-03-29 22:54:32 -0600

    by area filter map ready, don't know how to trigger stuff yet. play around with the map

--- VerticalPrototype/WelcomeToCalgary/WelcomeToCalgary/EventsMain.xaml.cs
+++ VerticalPrototype/WelcomeToCalgary/WelcomeToCalgary/EventsMain.xaml.cs
@@ -20,7 +20,8 @@ namespace WelcomeToCalgary
     /// </summary>
     public partial class EventsMain : UserControl
     {
-
+		string oneRegSelect = "";
+		
         StackPanel mainScroll;
         public EventsMain(StackPanel scroll)
         {
@@ -28,8 +29,86 @@ namespace WelcomeToCalgary
             mainScroll = scroll;
 
         }
- 	
+
+        private void filterArea_NE_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+			
+			 if (oneRegSelect == "NE")
+			{
+				buttNE_click.Opacity =0;
+				oneRegSelect = "";
+			}
+			else
+			{
+				buttNE_click.Content = "NE selected";
+				clearAreaButtOpac();
+				buttNE_click.Opacity=80;
+				oneRegSelect= "NE";
+			}
+			
+			// TODO: Add event handler implementation here.
+        }
+
+        private void filterArea_SE_click(object sender, System.Windows.RoutedEventArgs e)
+        {
+			 if (oneRegSelect == "SE")
+			{
+				buttSE_click.Opacity =0;
+				oneRegSelect = "";
+			}
+			else
+			{
+				buttSE_click.Content = "SE selected";
+				clearAreaButtOpac();
+				buttSE_click.Opacity=80;
+				oneRegSelect= "SE";
+			}
+			
+        }
 		
+		public void clearAreaButtOpac()
+		{
+			buttSE_click.Opacity=0;
+			buttNE_click.Opacity=0;
+			buttSW_click.Opacity=0;
+			buttNW_click.Opacity=0;
+		}
+
+		private void filterArea_SW_click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			 if (oneRegSelect == "SW")
+			{
+				buttSW_click.Opacity =0;
+				oneRegSelect = "";
+			}
+			else
+			{
+				buttSW_click.Content = "SW selected";
+				clearAreaButtOpac();
+				buttSW_click.Opacity=80;
+				oneRegSelect = "SW";
+			}
+			
+		}
+
+		private void filterArea_NW_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			if (oneRegSelect == "NW")
+			{
+				buttNW_click.Opacity =0;
+				oneRegSelect = "";
+			}
+			else
+			{
+				buttNW_click.Content = "NW selected";
+				clearAreaButtOpac();
+				buttNW_click.Opacity=80;
+				oneRegSelect = "NW";
+			}
+			// TODO: Add event handler implementation here.
+		}
+ 	
+	
        /* Old code for Horizontal Version of prototype; please ignore
 		
 		
