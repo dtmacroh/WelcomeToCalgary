d13a6a003228ca1515cdf1aba936adfca624d58c
huynjm <huynjm@ucalgary.ca>
2016-03-31 01:22:28 -0600

    finished events for area

--- VerticalPrototype/WelcomeToCalgary/WelcomeToCalgary/EventsMain.xaml.cs
+++ VerticalPrototype/WelcomeToCalgary/WelcomeToCalgary/EventsMain.xaml.cs
@@ -262,7 +262,7 @@ namespace WelcomeToCalgary
                                              "in a race to the finish line. Challenges are not based on athletic prowess or mental superiority; rather, they are designed for having a blast! " +
                                             "A post-race celebration including food, prizes and entertainment will be held at the finish line. Grown-ups play. Kids win.";
                             Event.Location = "Location: Boys And Girls Clubs Of Calgary \nAddress: 731 13 Ave NE, Calgary, AB T2E";
-                            Event.Website = "Website: www.raceforkids.ca/calgary";
+                            Event.Website = "Website: http://www.raceforkids.ca/calgary";
                         }
 
                         this.EventsHolder.Children.Add(Event);
@@ -378,12 +378,14 @@ namespace WelcomeToCalgary
                         }
                         else
                         {
-                            Event.Title = "Brazilian Heat";
-                            Event.Date = "Date : August 22, 2016";
-                            Event.Details = "This family-friendly event is a celebration of Brazilian culture. Spend the evening watching local and international artists performing" +
-                                            " Brazilian music and dance, such as capoeira. This is sure to be a high-energy, colourful evening. ";
-                            Event.Location = "Location: Hillhurst Sunnyside Community Centre  \nAddress: 1320 5 Ave. N.W. Calgary , AB";
-                            Event.Website = "Website: http://www.brazilian-heat.com/";
+                            Event.Title = "Supertrain";
+                            Event.Date = "Date : April 16 & April 17, 2016";
+                            Event.Details = "Calgary Model Railway Society presents Canada's Biggest and Best annual Model Train Show. Over 70,000 sq ft of operating model railroad layouts, displays, " +
+                                            "hobby shops, model manufacturers, railroad art, photos, clinics and live demos for beginners, ride-on trains, Garden railroads, Lionel, Lego, Thomas, Children's Play Area, and much more. " +
+                                            "Free Parking or take the CTrain to SUPERTRAIN (Saddletowne Station). adult - $10. youth (6-12) - $5. under 6 - free." +
+                                            " Bring a non-perishable food donation for the Calgary Food Bank and receive a dollar off admission";
+                            Event.Location = "Location: Genesis centre  \nAddress: 7555 Falconridge Blvd NE, Calgary, Alberta  T3J 0C9 ";
+                            Event.Website = "Website: http://www.supertrain.ca/";
                         }
 
                         this.EventsHolder.Children.Add(Event);
@@ -397,22 +399,22 @@ namespace WelcomeToCalgary
 
                         if (i == 0)
                         {
-                            Event.Title = "Fiestaval";
-                            Event.Date = "Date : July 22, 23 and 24, 2016";
-                            Event.Details = "Fiestaval is a free and family-oriented, multicultural arts and entertainment festival highlighting the Latin American Culture." +
-                                            " It will take place in the heart of downtown Calgary at the Calgary Olympic Plaza on July 22, 23 and 24 2016." +
-                                             " It is an opportunity to support Calgary’s Latin community while encouraging multiculturalism among all Calgarians.";
-                            Event.Location = "Location: Calgary Olympic Plaza \nAddress: 228 8 Ave SE, Calgary, AB T2P 2M5";
-                            Event.Website = "Website: http://fiestaval.ca/calgary/";
+                            Event.Title = "CCAG Spring Art Show and Sale";
+                            Event.Date = "Date :  April 9, 2016";
+                            Event.Details = "The Calgary Creative Arts Guild is hosting our Annual Spring Show & Sale on April 9, 2016 at the Montgomery Community Centre, 5003 16 Ave NW from 10am to 4pm. " +
+                                                "Featuring original works from over 20 local artists, the event will provide refreshments, door prizes and a raffle draw for an original framed watercolour painting." +
+                                                " Admission is free. Donations to the Veteran's Food Bank gratefully accepted.";
+                            Event.Location = "Location: Montgomery Community Centre \nAddress: 5003 16 Ave NW Calgary, AB T3B 0N2";
+                            Event.Website = "Website: http://www.chatterblock.com/events/215795/ccag-spring-art-show-and-sale/";
                         }
                         else
                         {
-                            Event.Title = "Brazilian Heat";
-                            Event.Date = "Date : August 22, 2016";
-                            Event.Details = "This family-friendly event is a celebration of Brazilian culture. Spend the evening watching local and international artists performing" +
-                                            " Brazilian music and dance, such as capoeira. This is sure to be a high-energy, colourful evening. ";
+                            Event.Title = "The Fibre Shindig";
+                            Event.Date = "Date : April 16 , 2016";
+                            Event.Details = "All things Fibre! Hand dyed, artisan made - yarn, knitting, embroidery, crochet, felting, spinning, fibre, tools, pottery, ceramics, fine art, jewellery, hand designed and sewn items. " + 
+                                            "Come and meet Alberta's best fibre artists and artisans. ";
                             Event.Location = "Location: Hillhurst Sunnyside Community Centre  \nAddress: 1320 5 Ave. N.W. Calgary , AB";
-                            Event.Website = "Website: http://www.brazilian-heat.com/";
+                            Event.Website = "Website: https://www.fibreshindig.com/";
                         }
 
                         this.EventsHolder.Children.Add(Event);
@@ -427,22 +429,22 @@ namespace WelcomeToCalgary
 
                         if (i == 0)
                         {
-                            Event.Title = "Fiestaval";
-                            Event.Date = "Date : July 22, 23 and 24, 2016";
-                            Event.Details = "Fiestaval is a free and family-oriented, multicultural arts and entertainment festival highlighting the Latin American Culture." +
-                                            " It will take place in the heart of downtown Calgary at the Calgary Olympic Plaza on July 22, 23 and 24 2016." +
-                                             " It is an opportunity to support Calgary’s Latin community while encouraging multiculturalism among all Calgarians.";
-                            Event.Location = "Location: Calgary Olympic Plaza \nAddress: 228 8 Ave SE, Calgary, AB T2P 2M5";
-                            Event.Website = "Website: http://fiestaval.ca/calgary/";
+                            Event.Title = "Bearspaw Spring Art & Craft Sale and Market";
+                            Event.Date = "Date : April 30 to May 1 , 2016";
+                            Event.Details = "Local artists, photographers, woodworkers, soap makers, jewellers, painters, ceramic artists, knitters, crafters as well as local small businesses come together for the first annual Spring Art Sale and Market! ";
+                            Event.Location = "Location: B & P Cycle & Sports \nAddress: 1717-52st SE Calgary, AB T2A 1V1";
+                            Event.Website = "Website: https://www.facebook.com/events/1674840002770147/";
                         }
                         else
                         {
-                            Event.Title = "Brazilian Heat";
-                            Event.Date = "Date : August 22, 2016";
-                            Event.Details = "This family-friendly event is a celebration of Brazilian culture. Spend the evening watching local and international artists performing" +
-                                            " Brazilian music and dance, such as capoeira. This is sure to be a high-energy, colourful evening. ";
-                            Event.Location = "Location: Hillhurst Sunnyside Community Centre  \nAddress: 1320 5 Ave. N.W. Calgary , AB";
-                            Event.Website = "Website: http://www.brazilian-heat.com/";
+                            Event.Title = "Walk to Fight Arthritis";
+                            Event.Date = "Date : June 5 , 2016";
+                            Event.Details = "The Walk to Fight Arthritis is the signature event of the Arthritis Society. It is a 1km or 5km fun-filled family event that unites "+
+                                            "communities in supporting the nearly 4.5 million Canadians living with Arthritis. The annual Walk will take place in 23 communities across Canada. "+
+                                            "The money raised throughout the Walk helps fun research, advocacy and solutions to improve the quality of life for those living with arthritis and their families."+
+                                            " To register as a participant or as a volunteer go online. ";
+                            Event.Location = "Location: Fish Creek Provincial Park  \nAddress: 15979 Bow Bottom Trail SE, Calgary, AB T2P 0Y8";
+                            Event.Website = "Website: http://walktofightarthritis.ca/";
                         }
 
                         this.EventsHolder.Children.Add(Event);
@@ -458,22 +460,22 @@ namespace WelcomeToCalgary
 
                         if (i == 0)
                         {
-                            Event.Title = "Fiestaval";
-                            Event.Date = "Date : July 22, 23 and 24, 2016";
-                            Event.Details = "Fiestaval is a free and family-oriented, multicultural arts and entertainment festival highlighting the Latin American Culture." +
-                                            " It will take place in the heart of downtown Calgary at the Calgary Olympic Plaza on July 22, 23 and 24 2016." +
-                                             " It is an opportunity to support Calgary’s Latin community while encouraging multiculturalism among all Calgarians.";
+                            Event.Title = "Square Dancing & Lessons";
+                            Event.Date = "Date :  March 1 to April 18, 2016";
+                            Event.Details = "Square dancing every Monday @ 8-10 PM (except Dec. 21 & 28) beginning Sep. 21 at Sunalta Community Hall, 1627-10th Ave. SW. $5 per dancer. " +
+                                            "Three free lessons for new dancers from 7:00 to 8:30 PM Mondays Sep. 21, 28 & Oct. 5; same location. No experience or partner required.";
                             Event.Location = "Location: Calgary Olympic Plaza \nAddress: 228 8 Ave SE, Calgary, AB T2P 2M5";
-                            Event.Website = "Website: http://fiestaval.ca/calgary/";
+                            Event.Website = "Website: http://www.chatterblock.com/events/198604/square-dancing-lessons/";
                         }
                         else
                         {
-                            Event.Title = "Brazilian Heat";
-                            Event.Date = "Date : August 22, 2016";
-                            Event.Details = "This family-friendly event is a celebration of Brazilian culture. Spend the evening watching local and international artists performing" +
-                                            " Brazilian music and dance, such as capoeira. This is sure to be a high-energy, colourful evening. ";
-                            Event.Location = "Location: Hillhurst Sunnyside Community Centre  \nAddress: 1320 5 Ave. N.W. Calgary , AB";
-                            Event.Website = "Website: http://www.brazilian-heat.com/";
+                            Event.Title = "Jayman BUILT MS Walk";
+                            Event.Date = "Date : Jun 12, 2016";
+                            Event.Details = "Join thousands of Calgarians for the 25th Annual Jayman BUILT MS Walk as we walk together to end MS. The Jayman BUILT MS Walk is more than a Walk, " +
+                                            "it’s a movement to end MS, and we need your help! Walk with us and 2000 of your closest friends on June 12, 2016 at Prince’s Island Park. Help us find a cure." +
+                                            " Help us bring hope to those living with MS. Every step matters.";
+                            Event.Location = "Location: Prince's Island Park  \nAddress: 698 Eau Claire Ave SW, Calgary, AB";
+                            Event.Website = "Website: http://mssoc.convio.net/site/TR?fr_id=4943&pg=entry";
                         }
 
                         this.EventsHolder.Children.Add(Event);
