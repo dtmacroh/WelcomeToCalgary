344f364a8e739de307068008971de47666225220
djarmstr1 <djarmstr@ucalgary.ca>
2016-04-01 15:16:49 -0600

    Aesthetic changes at temporary stopping point

--- VerticalPrototype/WelcomeToCalgary/WelcomeToCalgary/EventsMain.xaml.cs
+++ VerticalPrototype/WelcomeToCalgary/WelcomeToCalgary/EventsMain.xaml.cs
@@ -280,7 +280,7 @@ namespace WelcomeToCalgary
                         this.EventsHolder.Children.Add(Event);
                     }
                 }
-                else if (AgeTeen.IsSelected)
+                else if (AgeYA.IsSelected)
                 {
                     this.UpcomingBlock.Text = "EVENTS FOR YOUNG ADULTS";
 
